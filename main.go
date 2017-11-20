13bf3124839f0268a037af086b3222ebba07b5f9
timothy bone <timothybone@dhcp-169-233-142-211.cruznet.ucsc.edu>
2017-11-20 15:15:10 -0800

    puts files on desktop

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"net/url"
 
-	"github.com/BrianAllred/goydl"
+	"github.com/TableMountain/goydl"
 	"github.com/tjgq/clipboard"
 )
 
@@ -14,31 +14,43 @@ func main() {
 }
 
 func f() {
-	defer func() {
-		if r := recover(); r != nil {
-			fmt.Println("Recovered from bad URL", r)
-		}
-	}()
 	c := make(chan string, 1)
 	youtubeDl := goydl.NewYoutubeDl()
+
 	// go io.Copy(os.Stdout, youtubeDl.Stdout)
 	// go io.Copy(os.Stderr, youtubeDl.Stderr)
 	for {
 		clipboard.Notify(c)
 		got := <-c
-		println("got: ", got)
-		_, err := url.Parse(got)
+		youtubeDl.VideoURL = got
+		info, err := youtubeDl.GetInfo()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		println("got: ", got, "\nas: ", info.Title)
+		_, err = url.Parse(got)
 		if err != nil {
 			fmt.Println("URL recieved not valid: ", err)
 			continue
 		}
-		cmd, err := youtubeDl.Download(got)
-		if err != nil {
-			log.Print("that wasn't a good URL, ", err)
-		} else {
-			println("starting download")
-			cmd.Wait()
-			println("finished download")
-		}
+		path := fmt.Sprintf("~/Desktop/%s",info.Title)
+		youtubeDl.Options.Output.Value = path
+		go func() {
+			defer func() {
+				if r := recover(); r != nil {
+					fmt.Println("Recovered from bad URL", r)
+				}
+			}()
+			cmd, err := youtubeDl.Download(got)
+			youtubeDl.Options.Output.Value = info.Title
+			if err != nil {
+				log.Print("that wasn't a good URL, ", err)
+			} else {
+				println("starting download")
+				cmd.Wait()
+				println("finished download")
+			}
+		}()
 	}
 }
